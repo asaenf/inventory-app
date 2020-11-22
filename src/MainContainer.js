8470edd8e38974455f9ad368054ea275b7b3aef5
Asa <asa_enfors@hotmail.com>
2020-11-22 20:54:43 +0100

    Add summary page

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,9 +1,12 @@
-import { React, Fragment, Link } from "react";
+import { React, Fragment, Link, useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import Inventory from "./Inventory.js";
+import Summary from "./Summary.js";
 import { useRequireAuth } from "./use-require-auth.js";
 import { useRouter } from "./use-router.js";
 import { makeStyles } from "@material-ui/core/styles";
@@ -14,16 +17,38 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     marginRight: theme.spacing(2),
+    color: "white",
+  },
+  tab: {
+    color: "white",
+    indicator: "#D3D3D3",
   },
   title: {
     flexGrow: 1,
   },
 }));
 
+function MainPage(props) {
+  const tab = props.activeTab;
+
+  if (tab === 0) {
+    console.log("Render inventory");
+    return <Inventory />;
+  } else {
+    console.log("Render summary");
+    return <Summary />;
+  }
+}
+
 function MainContainer(props) {
   const auth = useRequireAuth();
   const router = useRouter();
   const classes = useStyles();
+  const [tab, setTab] = useState(0);
+
+  const handleTabClick = (event, newValue) => {
+    setTab(newValue);
+  };
 
   if (!auth.user) {
     console.log("User not authenticated, redirecting");
@@ -39,18 +64,22 @@ function MainContainer(props) {
   } else {
     console.log("Authenticated user ", auth.user.email);
     return (
-      <div>
+      <div className={classes.root}>
         <AppBar position="sticky">
           <Toolbar>
             <Typography className={classes.title}>
               Signed in as {auth.user.email}
             </Typography>
+            <Tabs value={tab} onChange={handleTabClick}>
+              <Tab label="Inventory" className={classes.tab} />
+              <Tab label="Summary" className={classes.tab} />
+            </Tabs>
             <Button className={classes.button} onClick={() => auth.signout()}>
               Signout
             </Button>
           </Toolbar>
         </AppBar>
-        <Inventory />
+        <MainPage activeTab={tab} />
       </div>
     );
   }
---
+++ src/Summary.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { forwardRef } from "react";
+import MaterialTable from "material-table";
+
+
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+import ErrorDialog from "./ErrorDialog.js";
+import firebase from "./FirebaseFunctions.js";
+
+var functions = firebase.functions();
+
+var getAllItems = functions.httpsCallable("getAllItems");
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+const tableIcons = {
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+export default function Summary() {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [error, setError] = useState("");
+  const [errorCode, setErrorCode] = useState("");
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false);
+
+  useEffect(() => {
+    getAllItems({})
+      .then((result) => {
+        // Read result of the Cloud Function.
+        var items = result.data.items;
+        console.log("calculating total of items ", items);
+        var summedItems = Object.values(
+          items.reduce((c, { item, quantity }) => {
+            c[item] = c[item] || { item, quantity: 0 };
+            c[item].quantity += quantity;
+            return c;
+          }, {})
+        );
+        setData(summedItems);
+        setDataLoaded(true);
+      })
+      .catch((error) => {
+        console.log("error getting data: ", error.message);
+        setError(error.message);
+        setErrorCode(error.code);
+        shiftErrorDialog(true);
+      });
+  });
+
+  let shiftErrorDialog = (isOpen) => {
+    console.log("set error dialog: ", isOpen);
+    if (!isOpen) {
+      setError("");
+      setErrorCode("");
+    }
+    setErrorDialogOpen(isOpen);
+  };
+
+  return (
+    <Container component="main">
+      {!dataLoaded ? (
+        <CircularProgress />
+      ) : (
+        <div style={{ maxWidth: "100%" }}>
+          <ErrorDialog
+            open={errorDialogOpen}
+            onClose={() => shiftErrorDialog(false)}
+            error={error + ". Error code: " + errorCode}
+          />
+          <MaterialTable
+            icons={tableIcons}
+            columns={[
+              { title: "Product", field: "item" },
+              { title: "Total", field: "quantity", type: "numeric" },
+            ]}
+            data={data}
+            options={{
+              filtering: true,
+            }}
+            title="Summary"
+          />
+        </div>
+      )}
+    </Container>
+  );
+}
