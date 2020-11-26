91f31d04e846b74c0ce03e0fe4a70ace6d57f12e
Asa <asa_enfors@hotmail.com>
2020-11-26 22:05:24 +0100

    Add summary data in tree structure
    Replace material table with material ui table to get more advanced tree functions in order to display actual data under summary data

--- src/Summary.js
+++ src/Summary.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { forwardRef } from "react";
-import MaterialTable from "material-table";
-
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Container from "@material-ui/core/Container";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 import ErrorDialog from "./ErrorDialog.js";
 import firebase from "./FirebaseFunctions.js";
@@ -23,36 +24,98 @@ import firebase from "./FirebaseFunctions.js";
 var functions = firebase.functions();
 
 var getAllItems = functions.httpsCallable("getAllItems");
+const collectionName = process.env.REACT_APP_FIRESTORE_TABLE;
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+const useRowStyles = makeStyles({
+  root: {
+    "& > *": {
+      borderBottom: "unset",
+    },
   },
 });
 
-const tableIcons = {
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.item}
+        </TableCell>
+        <TableCell align="right">{row.total}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Details
+              </Typography>
+              <Table size="small" aria-label="details">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Location</TableCell>
+                    <TableCell align="right">Comment</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.details.map((detailsRow) => (
+                    <TableRow key={detailsRow.location}>
+                      <TableCell>{detailsRow.quantity}</TableCell>
+                      <TableCell component="th" scope="row">
+                        {detailsRow.location}
+                      </TableCell>
+                      <TableCell align="right">{detailsRow.comment}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    item: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired,
+    details: PropTypes.arrayOf(
+      PropTypes.shape({
+        quantity: PropTypes.number.isRequired,
+        location: PropTypes.string.isRequired,
+        comment: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
 };
 
-const collectionName = process.env.REACT_APP_FIRESTORE_TABLE;
+function compareSummedItems(summedItem, otherSummedItem) {
+  if (summedItem.item < otherSummedItem.item) {
+    return -1;
+  }
+  if (summedItem.item > otherSummedItem.item) {
+    return 1;
+  }
+  return 0;
+}
 
 export default function Summary() {
-  const classes = useStyles();
   const [data, setData] = useState([]);
   const [dataLoaded, setDataLoaded] = useState(false);
   const [error, setError] = useState("");
@@ -70,12 +133,22 @@ export default function Summary() {
         var items = result.data.items;
         console.log("calculating total of items ", items);
         var summedItems = Object.values(
-          items.reduce((c, { item, quantity }) => {
-            c[item] = c[item] || { item, quantity: 0 };
-            c[item].quantity += quantity;
+          items.reduce((c, { item, quantity, location, comment }) => {
+            c[item] = c[item] || { item, total: 0 };
+            c[item].total += quantity;
+            if (c[item].item === item) {
+              if (!c[item].details) {
+                c[item].details = new Array();
+                c[item].details.push({ item, quantity, location, comment });
+              } else {
+                c[item].details.push({ item, quantity, location, comment });
+              }
+            }
             return c;
           }, {})
         );
+        summedItems.sort(compareSummedItems);
+        console.log("summarised items ", summedItems);
         setData(summedItems);
         setDataLoaded(true);
       })
@@ -107,19 +180,22 @@ export default function Summary() {
             onClose={() => shiftErrorDialog(false)}
             error={error + ". Error code: " + errorCode}
           />
-          <MaterialTable
-            icons={tableIcons}
-            columns={[
-              { title: "Product", field: "item", defaultSort: "asc" },
-              { title: "Total", field: "quantity", type: "numeric" },
-            ]}
-            data={data}
-            options={{
-              filtering: true,
-              sorting: true,
-            }}
-            title="Summary"
-          />
+          <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Product</TableCell>
+                  <TableCell align="right">Total</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((row) => (
+                  <Row key={row.item} row={row} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       )}
     </Container>
